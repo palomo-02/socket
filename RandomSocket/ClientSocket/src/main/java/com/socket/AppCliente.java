34933d50df46441bfe814016458b604b01b6c2be
jose <palomo.zambrano.jose.manuel@iescamas.es>
2026-01-22 21:17:16 +0100

    lo de las redes

--- RandomSocket/ClientSocket/src/main/java/com/socket/AppCliente.java
+++ RandomSocket/ClientSocket/src/main/java/com/socket/AppCliente.java
@@ -20,7 +20,7 @@ public class AppCliente
     	
         try {
         	//conectamos con el servidor
-        	Socket socket = new Socket("localhost",PORT);
+        	Socket socket = new Socket("192.168.1.67",PORT);  
         	
         	//Para enviar datos al server
         	PrintWriter salida = new PrintWriter(socket.getOutputStream(), true);
